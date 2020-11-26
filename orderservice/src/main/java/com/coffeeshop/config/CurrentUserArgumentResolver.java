702c051254a1b2376898ce9284ec2417698e2f18
dhaval <dmehta17@gmail.com>
2020-11-26 18:22:31 +0530

    Add spring-security and dummy User argument resolver

---
+++ orderservice/src/main/java/com/coffeeshop/config/CurrentUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.coffeeshop.config;
+
+import com.coffeeshop.domain.User;
+import com.coffeeshop.domain.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+@Component
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return methodParameter.getParameter().getType() == User.class;
+    }
+
+    @Override
+    public Object resolveArgument(@NonNull MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, @NonNull NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) {
+        return userRepository.getOne(1L);
+    }
+}
---
+++ orderservice/src/main/java/com/coffeeshop/config/CurrentUserArgumentResolverConfiguration.java
@@ -0,0 +1,20 @@
+package com.coffeeshop.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Component
+public class CurrentUserArgumentResolverConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private CurrentUserArgumentResolver currentUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentUserArgumentResolver);
+    }
+}
